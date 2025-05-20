c6e919a7188b19743c7164d90732e76b07de5890
kxghanta <kranthi.ghanta1@copart.com>
2025-05-20 03:31:34 -0500

    test1

--- CreateTrip.java
+++ CreateTrip.java
@@ -1,5 +1,5 @@
 public class CreateTrip {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Tow type changed to Heavy");
     }
 }
