4281502e85d28a387fda72915c55e8f94d40f8b9
kxghanta <kranthi.ghanta1@copart.com>
2025-05-29 16:07:25 -0500

    test milcheck api key in master

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test  {
     public static void main(String[] args) {
         String towType = "Medium";
         System.out.println("Tow type changed from Medium to high!" + towType);
+        System.out.println("Test milcheck api key");
     }
 }
