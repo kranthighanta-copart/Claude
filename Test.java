db343c2b42d24fd444918c8a0c8381f8cf07284b
kxghanta <kranthi.ghanta1@copart.com>
2025-05-29 15:59:11 -0500

    test code for best practices

---
+++ Test.java
@@ -0,0 +1,6 @@
+public class Test  {
+    public static void main(String[] args) {
+        String towType = "Medium";
+        System.out.println("Tow type changed from Medium to high!" + towType);
+    }
+}
